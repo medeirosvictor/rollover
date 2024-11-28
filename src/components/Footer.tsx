554281b29301b3aa4629db591406591211226dbb
Victor Medeiros <victoramedeiros@gmail.com>
2024-11-27 20:33:26 -0500

    vuejs refactoring

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white py-4 text-center">
-      <p>© 2023 Your Company. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
